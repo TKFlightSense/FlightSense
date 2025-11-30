e1dce3cf91a84b0e96c7ed5cfa123e90c14538f8
yazralkan <115217813+yazralkan@users.noreply.github.com>
2025-11-30 23:45:46 +0300

    add: statistic methods for manager

--- services/statistics_service.py
+++ services/statistics_service.py
@@ -96,4 +96,110 @@ class StatisticsService:
             weekly_stats_dict[key] = {"positive": positive_counts, "negative": negative_counts}
 
         return weekly_stats_dict
-            
+
+    def get_department_monthly_stats(self, department_name: str, date_from: datetime):
+        """
+        date_from: 2025-11-01 00:00:00
+        date_to: 2025-12-01 00:00:00
+        """
+        monthly_stats_dict = {}
+        for i in range(4):
+            key = f"week_{i+1}"
+            internal_date_from = date_from + timedelta(days=i)
+            internal_date_to = date_from + timedelta(days=i+1)
+            positive_counts = self.db.get_department_positive_counts(department_name, internal_date_from, internal_date_to)
+            negative_counts = self.db.get_department_negative_counts(department_name, internal_date_from, internal_date_to)
+            monthly_stats_dict[key] = {"positive": positive_counts, "negative": negative_counts}
+
+        return monthly_stats_dict
+
+    def get_department_yearly_stats(self, department_name: str, date_from: datetime):
+        """
+        date_from: 2024-12-01 00:00:00
+        date_to: 2025-12-01 00:00:00
+        """
+        yearly_stats_dict = {}
+        for i in range(12):
+            key = f"month_{i+1}"
+            internal_date_from = date_from + timedelta(days=i)
+            internal_date_to = date_from + timedelta(days=i+1)
+            positive_counts = self.db.get_department_positive_counts(department_name, internal_date_from, internal_date_to)
+            negative_counts = self.db.get_department_negative_counts(department_name, internal_date_from, internal_date_to)
+            yearly_stats_dict[key] = {"positive": positive_counts, "negative": negative_counts}
+
+        return yearly_stats_dict
+
+    def get_manager_sentiment_distribution(self, date_from: datetime, date_to: datetime):
+        positive_counts = self.db.get_manager_positive_counts(date_from, date_to)
+        negative_counts = self.db.get_manager_negative_counts(date_from, date_to)
+        neutral_counts = self.db.get_manager_neutral_counts(date_from, date_to)
+
+        sentiment_counts_dict = {"positive": positive_counts, "negative": negative_counts, "neutral": neutral_counts}
+        sentiment_percentage_dict = self._to_percentage(sentiment_counts_dict)
+        return {
+            "counts": sentiment_counts_dict,
+            "percentage": sentiment_percentage_dict
+        }
+
+    def get_manager_priority_distribution(self, date_from: datetime, date_to: datetime):
+        high_counts = self.db.get_manager_high_counts(date_from, date_to)
+        medium_counts = self.db.get_manager_medium_counts(date_from, date_to)
+        low_counts = self.db.get_manager_low_counts(date_from, date_to)
+
+        priority_counts_dict = {"high": high_counts, "medium": medium_counts, "low": low_counts}
+        priority_percentage_dict = self._to_percentage(priority_counts_dict)
+        return {
+            "counts": priority_counts_dict,
+            "percentage": priority_percentage_dict
+        }
+
+    def get_manager_total_review_count(self, date_from: datetime, date_to: datetime):
+        return self.db.get_manager_total_count(date_from, date_to)
+
+    def get_manager_weekly_stats(self, date_from: datetime):
+        """
+        date_from: 2025-12-01 00:00:00
+        date_to: 2025-12-08 00:00:00 #anlaması kolay olsun diye
+        """
+        weekly_stats_dict = {}
+        for i in range(7):
+            key = f"day_{i + 1}"
+            internal_date_from = date_from + timedelta(days=i)
+            internal_date_to = date_from + timedelta(days=i + 1)
+            positive_counts = self.db.get_manager_positive_counts(internal_date_from, internal_date_to)
+            negative_counts = self.db.get_manager_negative_counts(internal_date_from, internal_date_to)
+            weekly_stats_dict[key] = {"positive": positive_counts, "negative": negative_counts}
+
+        return weekly_stats_dict
+
+    def get_manager_monthly_stats(self,date_from: datetime):
+        """
+        date_from: 2025-11-01 00:00:00
+        date_to: 2025-12-01 00:00:00
+        """
+        monthly_stats_dict = {}
+        for i in range(4):
+            key = f"week_{i + 1}"
+            internal_date_from = date_from + timedelta(days=i)
+            internal_date_to = date_from + timedelta(days=i + 1)
+            positive_counts = self.db.get_manager_positive_counts(internal_date_from, internal_date_to)
+            negative_counts = self.db.get_manager_negative_counts(internal_date_from, internal_date_to)
+            monthly_stats_dict[key] = {"positive": positive_counts, "negative": negative_counts}
+
+        return monthly_stats_dict
+
+    def get_manager_yearly_stats(self, date_from: datetime):
+        """
+        date_from: 2024-12-01 00:00:00
+        date_to: 2025-12-01 00:00:00
+        """
+        yearly_stats_dict = {}
+        for i in range(12):
+            key = f"month_{i + 1}"
+            internal_date_from = date_from + timedelta(days=i)
+            internal_date_to = date_from + timedelta(days=i + 1)
+            positive_counts = self.db.get_manager_positive_counts(internal_date_from, internal_date_to)
+            negative_counts = self.db.get_manager_negative_counts(internal_date_from, internal_date_to)
+            yearly_stats_dict[key] = {"positive": positive_counts, "negative": negative_counts}
+
+        return yearly_stats_dict
